939d770ead2ef28e769ee4dddaf4ad0f73351ad8
saulo <saulo@beast>
2021-04-11 20:18:50 -0300

    Adaptacion script 352a-353a 251
    El largo actual de las columnas es mayor al largo que se quiere aplicar

--- migration-historic/352a-353a/postgresql/251_BF2031818.sql
+++ migration-historic/352a-353a/postgresql/251_BF2031818.sql
@@ -15,12 +15,12 @@ insert into t_alter_column values('ad_process_para','ColumnName','VARCHAR(30)',n
 UPDATE AD_Column SET FieldLength=30,Updated=TO_TIMESTAMP('2008-07-31 21:17:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=2602
 ;
 
-insert into t_alter_column values('ad_element','ColumnName','VARCHAR(30)',null,'NULL')
+-- insert into t_alter_column values('ad_element','ColumnName','VARCHAR(30)',null,'NULL')
 ;
 
 UPDATE AD_Column SET FieldLength=30,Updated=TO_TIMESTAMP('2008-07-31 21:17:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=116
 ;
 
-insert into t_alter_column values('ad_column','ColumnName','VARCHAR(30)',null,'NULL')
+-- insert into t_alter_column values('ad_column','ColumnName','VARCHAR(30)',null,'NULL')
 ;
 
