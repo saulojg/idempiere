eb42fa6822b18d5bb1bbe7043af5786d8197d8cd
saulo <saulo@beast>
2021-04-12 16:42:26 -0300

    Agrego checkpoints a script de migración

--- migration-historic/migrate-orbital.sh
+++ migration-historic/migrate-orbital.sh
@@ -2,14 +2,19 @@
 
 # Author Saulo Gil <saulo.gil@orbital.com.ar>
 
-for i in $(ls *.sql | sort );
+for i in $(ls *.sql | egrep -v "^checkpoint.*" | sort );
 do
+	if [ -e checkpoint.$i ];
+	then
+		continue;
+	fi;
+	echo executing $i
         psql --single-transaction -f $i $*
         if [  $? -ne 0 ];
         then
                 echo "failed at $i";
                 exit;
         fi;
-
+	touch checkpoint.$i
 done;
 
