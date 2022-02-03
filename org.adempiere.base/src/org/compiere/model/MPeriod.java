af992b81c4e83f52509a73d36377896cfbecb8a4
dREHER <jorge.dreher@gmail.com>
2022-02-03 14:03:09 -0300

    Se restringe la creacion de periodos automaticos a anio actual y al proximo.

--- org.adempiere.base/src/org/compiere/model/MPeriod.java
+++ org.adempiere.base/src/org/compiere/model/MPeriod.java
@@ -1039,7 +1039,7 @@ public class MPeriod extends X_C_Period implements ImmutablePOSupport
 				if(C_Year_ID > -1) {
 					
 					// Romper para que no carguen cualquier a#o en los documentos....
-					if(anio < anioActual || (anio - anioActual) > 2)
+					if(anio < anioActual || (anio - anioActual) > 1)
 						return false;
 					
 					MYear y = new MYear(Env.getCtx(), C_Year_ID, null);
